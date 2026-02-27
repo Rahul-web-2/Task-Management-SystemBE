9bb96cb836cbc08a385b7180dbbd676e1645c84d
Rahul Lakhchaura <rlakhchaura3@gmail.com>
2026-02-27 19:12:10 +0530

    v1.3

--- src/main/java/com/TaskManagementSystem/controller/TaskController.java
+++ src/main/java/com/TaskManagementSystem/controller/TaskController.java
@@ -1,11 +1,32 @@
 package com.TaskManagementSystem.controller;
 
+import com.TaskManagementSystem.model.Task;
+import com.TaskManagementSystem.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class TaskController {
 
+    private final TaskService taskService;
+
+    @PostMapping("/user/{userId}")
+    public Task createTask(@PathVariable Long userId,
+                           @RequestBody Task task) {
+        return taskService.createTask(userId, task);
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+    }
 }
--- src/main/java/com/TaskManagementSystem/service/TaskService.java
+++ src/main/java/com/TaskManagementSystem/service/TaskService.java
@@ -1,4 +1,35 @@
 package com.TaskManagementSystem.service;
 
+import com.TaskManagementSystem.model.Task;
+import com.TaskManagementSystem.model.User;
+import com.TaskManagementSystem.repository.TaskRepository;
+import com.TaskManagementSystem.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class TaskService {
+
+    private final TaskRepository taskRepository;
+    private final UserRepository userRepository;
+
+
+    public Task createTask(Long userId, Task task) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        task.setUser(user);
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
 }
